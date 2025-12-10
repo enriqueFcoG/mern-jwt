cbdfdde7600f20955ad0e731a5082728b5ca2029
enriqueFcoG <enrikefrancisco@gmail.com>
2025-12-09 20:46:13 -0800

    frontend/feat: enpoint "me" added to users service

--- frontend/src/services/users.service.ts
+++ frontend/src/services/users.service.ts
@@ -5,7 +5,6 @@ import { cookies } from "next/headers";
 export const  getUsers = async () => {
   const cookieStore = cookies();
   const token = (await cookieStore).get("access_token")?.value;
-
   const res = await fetch(`${process.env.BACKEND_URL}/api/users`, {
     method: "GET",
     headers: {
@@ -21,7 +20,25 @@ export const  getUsers = async () => {
 
   return users
 }
+export const getCurrentUser = async () => {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("access_token")?.value;
 
+  const res = await fetch(`${process.env.BACKEND_URL}/api/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    cache: "no-store",
+  });
+  
+  if (!res.ok) return null;
+
+  const users = await res.json();
+
+  return users
+}
 export const getUser = async (id: number) => {
   const cookieStore = cookies();
   const token = (await cookieStore).get("access_token")?.value;
