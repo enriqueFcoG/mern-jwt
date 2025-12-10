ea5d3935a181b30d041d89a94a8f5ddabfa0a267
enriqueFcoG <enrikefrancisco@gmail.com>
2025-12-09 21:36:35 -0800

    backend/refactor: login endpoint changed (now return tokens only)

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -31,10 +31,7 @@ export class AuthService {
   async login(user: any) {
     const payload = { username: user.email, sub: user.id };
     const refresh_token = this.jwtService.sign(payload, { expiresIn: '7d' });
-    // await this.usersService.updateRefreshToken(user.id, refresh_token);
     return {
-      id: user.id,
-      email: user.email,
       access_token: this.jwtService.sign(payload),
       refresh_token
     };
