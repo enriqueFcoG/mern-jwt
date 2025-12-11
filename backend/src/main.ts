33c9f0458b63522866cbae8e2429d7b0494178cc
enriqueFcoG <enrikefrancisco@gmail.com>
2025-12-11 01:14:15 -0800

    backend/fix: disable swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,17 +22,17 @@ async function bootstrap() {
   })
   app.setGlobalPrefix('api');
 
-  const config = new DocumentBuilder()
-    .setTitle('API Documentation')
-    .setDescription('Documentación de la API con Swagger en NestJS')
-    .setVersion('1.0')
-    .addBearerAuth() // Agrega soporte para JWT en la UI
-    .build();
+  // const config = new DocumentBuilder()
+  //   .setTitle('API Documentation')
+  //   .setDescription('Documentación de la API con Swagger en NestJS')
+  //   .setVersion('1.0')
+  //   .addBearerAuth() // Agrega soporte para JWT en la UI
+  //   .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document, {
-    swaggerOptions: { persistAuthorization: true }, // Mantiene el token al recargar
-  });
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('docs', app, document, {
+  //   swaggerOptions: { persistAuthorization: true }, // Mantiene el token al recargar
+  // });
   
   await app.listen(process.env.PORT ?? 3001);
 }
