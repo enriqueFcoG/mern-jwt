7925c58fb119ddf65ac4e33460169a8f2d5cdac5
enriqueFcoG <enrikefrancisco@gmail.com>
2026-01-14 11:44:45 -0800

    frontend/feat: api route handlers added

--- frontend/app/api/me/route.ts
+++ frontend/app/api/me/route.ts
@@ -3,13 +3,13 @@ import { cookies } from "next/headers";
 export async function GET() {
   const cookieStore = cookies();
   const cookieHeader = cookieStore.toString();
-console.log("cookies headers ", cookieHeader)
+
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/me`, {
     headers: { Cookie: cookieHeader },
     cache: "no-store",
   });
 
   const data = await res.json();
-  console.log("RESPONSE DATA FROM BACKEND ", data)
+  
   return new Response(JSON.stringify(data), { status: res.status });
 }
